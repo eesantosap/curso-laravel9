ebf6a84654ec1d6638f38c44b9dbb9f0cae45511
Evaldo Expedito <eesantosap@hotmail.com>
2022-03-27 11:28:17 -0300

    second change

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUpdateUserFormRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        //dd('UserController@index');
+
+        //$users = User::where('name', 'LIKE', "%{$request->search}%")->get();
+
+        //igual a pesquisa acima
+
+        $search = $request->search;
+        $users = User::where(function ($query) use ($search) {
+
+            if ($search) {
+                $query->where('email', $search);
+                $query->orWhere('name', 'LIKE', "%{$search}%");
+            }
+            
+        })->get();
+
+        return view('users.index', compact('users'));
+
+    }
+
+    public function show($id)
+    {
+        
+        //$user = User::where('id', $id)->first();
+
+        if (!$user = User::find($id))
+            return redirect()->route('users.index');
+        
+        //dd($user);
+        //dd('users.show', $id);
+
+        return view('users.show', compact('user'));
+
+    }
+
+    public function create()
+    {
+
+        return view('users.create');
+
+    }
+
+    public function store(StoreUpdateUserFormRequest $request)
+    {
+
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+
+        $user = User::create($data);
+
+        return redirect()->route('users.index');
+
+        //uma outra alternativa
+        //return redirect()->route('users.show, $user->id');
+
+        //outra forma não recomendada
+        //$user = new User;
+        //$user->name = $request->name;
+        //$user->email = $request->email;
+        //$user->password = $request->password;
+        //|$user->save()
+
+    }
+
+    public function edit($id)
+    {
+
+        if (!$user = User::find($id))
+            return redirect()->route('users.index');
+
+        return view('users.edit', compact('user'));
+
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        if (!$user = User::find($id))
+            return redirect()->route('users.index');
+
+        $data = $request->only('name', 'email');
+        if ($request->password)
+            $data['password'] = bcrypt($request->password);
+
+        $user->update($data);
+
+        return redirect()->route('users.index');
+
+        //$user->update($request->all());
+
+    }
+
+}
---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,12 @@
+﻿@extends('layouts.app')
+
+@section('title', 'Detalhes do Usuário')
+
+@section('content')
+    <h1>Lista de Usuários {{ $user->name }}</h1>
+    <ul>
+        <li>{{ $user->name }}</li>
+        <li>{{ $user->email }}</li>
+        <li>{{ $user->created_at }}</li>
+    </ul>
+@endsection
